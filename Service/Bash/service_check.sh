10ab2f8f0c30f20688d3638650def11c32d8165e
Bader <zhanghong.personal@outlook.com>
2023-01-30 21:45:15 +0800

    change file format
    CRLF -> LF

--- Service/Bash/service_check.sh
+++ Service/Bash/service_check.sh
@@ -8,6 +8,7 @@
 #
 # release nodes:
 #   2022.06.14 - first release
+#   2023.01.30 - change file format CRLF -> LF
 #################################################################
 
 # User variables
